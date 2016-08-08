e797f6f8fc0ea0cf1d5d800d016b31b411e12002
joshuaavalos74 <joshuaavalos74@gmail.com>
2016-08-08 08:39:35 -0600

    cambioisosososossssafd

--- Radio.py
+++ Radio.py
@@ -1,7 +1,7 @@
 class Radio():
 	def __init__(self, marca):
 		self.marca=marca
-		self.encendido=True
+		self.encendido=False
 		self.emisoraFM=87
 		self.emisoraAM=300
 		self.volumen=0
@@ -14,13 +14,13 @@ class Radio():
 		self.encendido=False
 
 	def subir_volumen(self):
-		if volumen>=100:
+		if self.volumen>=100:
 			self.volumen=100
 		else:
 			self.volumen+=5
 
 	def bajar_volumen(self):
-		if volumen<=0:
+		if self.volumen<=0:
 			self.volumen=0
 		else:
 			self.volumen-=5
@@ -36,6 +36,18 @@ class Radio():
 				self.emisoraAM=300
 			else:
 				self.emisoraAM+=40
+	def bajar_estacion(self):
+		if self.en_FM:
+			if self.emisoraFM>107:
+				self.emisoraFM=87
+			else:
+				self.emisoraFM-=0.5
+		else:
+			if self.emisoraAM>1300:
+				self.emisoraAM=300
+			else:
+				self.emisoraAM-=40				
+
 	def cambio_emisora(self):
 		if self.en_FM:
 			self.en_FM=False
--- controlradio.py
+++ controlradio.py
@@ -7,17 +7,32 @@ print(Pradio.en_FM)
 print(Pradio.volumen)
 while desea_continuar:
 	if Pradio.encendido:
-		opc=input("1.Apagar\n2.Subir/Bajar Volumen\n3.Subir/Bajar Estacion\n4.Cambiar Emisora\n ")
+		opc=int(input("1.Apagar\n2.Subir/Bajar Volumen\n3.Subir/Bajar Estacion\n4.Cambiar Emisora\n \n"))
 		if opc==1:
 			Pradio.encendido=False
 		if opc==2:
-			Pradio.volumen
+			res=int(input("1.Subir volumen\n 2.Bajar volumen "))
+			if res==1:
+				Pradio.subir_volumen()
+			if res==2:
+				Pradio.baja_volumen()	
 		if opc==3:
-			Pradio.en_FM		
+			vol=int(input("1.Subir estacion\n 2.Bajar estacion "))	
+			if vol==1:
+				Pradio.subir_estacion()
+			if vol==2:
+				Pradio.bajar_estacion()	
 	else:
-		opc=input("1.Encender:\n2.Salir:")
+		opc=int(input("1.Encender:\n2.Salir:\n\n"))
 		if opc==1:
 			Pradio.encendido=True
 		else:	
 			desea_continuar=False	
+print(Pradio.marca)
+print(Pradio.encendido)
+print(Pradio.en_FM)
+print(Pradio.volumen)
+print(Pradio.emisoraFM)
+print(Pradio.emisoraAM)
+
 		
